1566ef978e2591e0f804b4f2f69dba450a2f8b57
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-07-24 11:48:14 +0100

    changing to tailwindcss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import './pages.css'
 
 function Login() {
-  return (
-    <div className='Login'>
-        <div className='login_left'>
-            <div className='log_left_top'>
-                <a href="#" className='log_logo'>Logo</a>
-                <a href="" className='log_ret'>
+  return ( 
+    <div className='Login-container w-full flex '>
+        <div className='flex flex-col w-7/12  m-0   pt-5 '>
+            <div className='flex justify-between mx-16 '>
+                <a href="#" className='log_logo text-xl '>Logo</a>
+                <a href="" className='log_ret  w-24  px-3 py-1 rounded-2xl border border-blue-500 hover:text-white'>
                     Retour
                 </a>
             </div>
-            <div className="log_left_body">
-                <div className="log_bienv">
+            <div className=" mt-40 mx-10 ">
+                <div className=" leading-snug text-4xl mb-4">
                     Bienvenue au <span style={{color:'#319CFF'}}>TNM</span> Admin<br/>Authentification.
                 </div>
-                <div className='log_form'>
-                    <form className='log_inner_form'>
-                        <input type="text" className='log_login' placeholder="Nom d'utilisateur..." name="" id="" /><br />
-                        <input type="password" className='log_mdp' placeholder="Mot de passe..." name="" id="" /><br />
-                        <button type='submit'>Se connecter</button>
+                <div className='form-container'>
+                    <form className='log_inner_form '>
+                        <input type="text" className='log_login rounded-xl w-2/4 pl-20 h-14  mt-4 text-base ' placeholder="Nom d'utilisateur..." name="" id="" /><br />
+                        <input type="password" className='log_mdp rounded-xl w-2/4 pl-20 h-14 mt-4 text-base' placeholder="Mot de passe..." name="" id="" /><br />
+                        <button type='submit' className='text-white mt-8 w-2/5 h-14 text-xl rounded-full   hover:shadow-none'>Se connecter</button>
                     </form>
                 </div>
             </div>
         </div>
-        <div className='login_right'></div>
+        <div className='login_right w-5/12  '></div>
     </div>
   )
 }

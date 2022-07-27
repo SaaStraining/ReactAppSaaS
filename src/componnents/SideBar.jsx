205e9beac33e46079a86c8d4c39afb35b53cd872
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-07-27 21:55:30 +0100

    styling sidebar initial style

--- src/componnents/SideBar.jsx
+++ src/componnents/SideBar.jsx
@@ -1,32 +1,70 @@
 import React from 'react'
+import { useState } from 'react';
 // import { Dropdown,Flowbite, DarkThemeToggle } from 'flowbite-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee ,faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { Flowbite } from 'flowbite-react';
+import { DarkThemeToggle } from 'flowbite-react';
 function SideBar() {
-
+  const [toggle,setToggle] = useState(false);
+  const Show = (e) =>{
+    e.preventDefault();
+    setToggle(!toggle);
+  }
   return (
 
-    <aside className='w-64 side-nav h-screen overflow-y-auto py-4 px-3 rounded dark:bg-gray-800 '>
+    <aside className='w-1/5 side-nav h-screen overflow-y-auto pb-4  rounded dark:bg-gray-800 '>
        <div className='w-full m-0'>
-          <div className='side-header w-full grid place-items-center h-10 text-center  m-0 mb-4'>
-            <h3 className='m-0'>LOGO</h3>
+          <div className='side-header  w-full grid place-items-center h-12 text-center  m-0 mb-4'>
+            <h3 className='m-0 text-white font-extrabold'>LOGO</h3>
           </div>
-          <div className='flex justify-center w-100 text-black  h-auto mb-11 text-white'>
-                <h3 className='border'>Ressources Humain </h3>
-                <FontAwesomeIcon icon={faAngleDown}  className=" mt-1 border text-blue-500 ml-3 "/>
+          <div className='w-100  h-auto mb-11 text-white'>
+              <div className="flex justify-center text-white">
+                  <h3 className=''>Ressources Humain </h3>
+                <FontAwesomeIcon icon={faAngleDown}  className=" mt-1  text-white ml-3 " onClick={Show}/>
+              </div>
                 {/* <FontAwesomeIcon icon="fa-solid fa-angle-down" /> */}
-             
+                <div>
+                    { 
+                      toggle ? 
+                        <div className='flex  border justify-center w-100 h-auto mb-11 text-white'>
+                            ha zebi 
+                        </div> : null
+                  }
+                </div>
             </div>
-            <div className='flex justify-center w-100 text-black  h-auto mb-11 text-white'>
-            <h3 className='border'>Ressources Materials </h3>
-                <FontAwesomeIcon icon={faAngleDown}  className=" mt-1 border text-blue-500 ml-3 "/>
-              
+            <div className='w-100  h-auto mb-11 text-white'>
+              <div className="flex justify-center text-white">
+                  <h3 className=''>Ressources Materiels </h3>
+                <FontAwesomeIcon icon={faAngleDown}  className=" mt-1  text-white ml-3 " onClick={Show}/>
+              </div>
+                {/* <FontAwesomeIcon icon="fa-solid fa-angle-down" /> */}
+                <div>
+                    { 
+                      toggle ? 
+                        <div className='flex  border justify-center w-100 h-auto mb-11 text-white'>
+                            ha zebi 
+                        </div> : null
+                  }
+                </div>
             </div>
-            <div className='flex justify-center w-100 text-black  h-auto mb-11 text-white'>
-            <h3 className='border'>Gestion des Produits </h3>
-                <FontAwesomeIcon icon={faAngleDown}  className=" mt-1 border text-blue-500 ml-3 "/>
+            <div className='w-100  h-auto mb-11 text-white'>
+              <div className="flex justify-center text-white">
+                  <h3 className=''>Gestion des Produits </h3>
+                <FontAwesomeIcon icon={faAngleDown}  className=" mt-1  text-white ml-3 " onClick={Show}/>
+              </div>
+                {/* <FontAwesomeIcon icon="fa-solid fa-angle-down" /> */}
+                <div>
+                    { 
+                      toggle ? 
+                        <div className='flex  border justify-center w-100 h-auto mb-11 text-white'>
+                            ha zebi 
+                        </div> : null
+                  }
+                </div>
+            </div>
        
-            </div> 
+           
        </div>
 
      

0a164b87042077a1e81b33b7e5076f51b57e2a74
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-08-03 21:40:33 +0100

    fixing navbar and side bar

--- src/pages/ControlPanel.jsx
+++ src/pages/ControlPanel.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Routes,Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 import AddHumanResource from "../componnents/HumanRessources/AddHumanResource";
 import NavBar from "../componnents/NavBar";
 import SideBar from "../componnents/SideBar";
 
 function ControlPanel() {
   return (
-    <div className="flex">
-        <SideBar/>
-        <div className="block w-4/5 ml-72">
-            <NavBar/>
-            <div className=" text-center  ">
-                <Outlet />
-            </div>
+    <div className="flex scrollbar-hide ">
+      <SideBar />
+      <div className="block w-4/5 ml-72 scrollbar-hide  ">
+        <NavBar />
+        <div className=" text-center mt-20  ">
+          <div className="scrollbar-hide">
+            <Outlet />
+          </div>
         </div>
+      </div>
     </div>
   );
 }

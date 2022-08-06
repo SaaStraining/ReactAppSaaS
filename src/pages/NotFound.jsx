df6430702d59b1f09799c70ce52c1bffc7f3076c
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-08-06 22:13:00 +0100

    adding not found images

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import NotFound404 from '../assets/illustrations/NotFound.jpg'
 function NotFound() {
   return (
-    <div>NotFound</div>
+   <img src={NotFound404} className="border " alt="" />
   )
 }
 

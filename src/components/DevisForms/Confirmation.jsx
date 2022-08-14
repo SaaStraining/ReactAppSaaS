0590f0ff91f0f01173dbc00caf96a49aabef28b0
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-08-14 03:21:13 +0100

    confirmation

--- src/components/DevisForms/Confirmation.jsx
+++ src/components/DevisForms/Confirmation.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Confirmation = () => {
+const Confirmation = ({ prevStep, nextStep, values }) => {
+  const Continue = (e) => {
+    e.preventDefault();
+    nextStep();
+  };
+  const Previous = (e) => {
+    e.preventDefault();
+    prevStep();
+  };
   return (
-    <div>Confirmation</div>
-  )
-}
+    <div>
+      <form action="">
+        <p>displaying data here for confirmation</p>
+        <button onClick={Previous}>Retour</button>
+        <button onClick={Continue}>Suivant</button>
+      </form>
+    </div>
+  );
+};
 
 export default Confirmation;

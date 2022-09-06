b7701b94442dd2d28c8bcf252b9dee63f0408f0e
Jemmal soufiane <jemmalsoufiane@gmail.com>
2022-09-06 21:39:18 +0100

    fewe changes on profile page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const Account = () => {
   return (
-    <div>Account</div>
-  )
-}
+    <div className="w-5/6 mx-auto border ">
+      <div className="">
+        <p className="text-left pb-3">Gérer Votre Compte</p>
+        <div className="flex  bg-primary w-5/6 mx-auto">
+          <img src="" alt="" />
+          <div className="text-lg text-white">
+            <p className="text-justify">Bienvenue au panel de configuration de votre profil</p>
+            <p className="text-left">Jhon Doe</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Account;
